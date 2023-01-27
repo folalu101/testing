3b62db48f717ce2a5c5ecd10c680591f36833694
A.Adefolalu B <adefolalucode101@gmail.com>
2023-01-27 20:24:18 +0100

    loop through the element to dispaly all index in console

--- src/app.js
+++ src/app.js
@@ -2,7 +2,13 @@
 
 // Arrow function without parameter
 set = () =>{
-    alert("clicked");
+    let element = document.getElementsByName('cssProperty');
+    console.log(element);
+
+    // for loop
+    for ( let i = 0; i < element.length; i++){
+        console.log(element[i].value);
+    }
 };
 
 // Normal function
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 // main.js for UX design
 
 // create an event listener using vanilla
+window.onload=function(){
 document.getElementById('set').addEventListener('click', set);
+
+
+
+
+};
